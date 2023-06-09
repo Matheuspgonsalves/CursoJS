93791dc7c5861cfaa1ac1d29a1d8cc3a1ced1923
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2023-06-08 21:18:07 -0300

    Reforçando conceito de funções em JavaScript

---
+++ aula16/func02.js
@@ -0,0 +1,8 @@
+function soma(m=0,n=0) {
+    s = m + n
+    return s
+}
+
+res = soma(5);
+console.log(res)
+
---
+++ aula16/func03.js
@@ -0,0 +1,7 @@
+let v = function (x) {
+    mult = x*2
+    return mult
+}
+ 
+
+ console.log(v(5))
